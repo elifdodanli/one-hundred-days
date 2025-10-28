ed4990a82c2d508d0520f643da56ebe471d2f705
Elif <elfdodanli@gmail.com>
2025-10-28 22:48:16 +0300

    add: implement ElectricCar and Truck subclasses with unique properties and methods for day 7

--- flutter/day_six.dart
+++ flutter/day_six.dart
@@ -1,5 +1,6 @@
-// 1. Class Definition - The "Blueprint"
-// This is the plan that defines what a Car should be.
+// --- SUPERCLASS (PARENT) ---
+// This is the base class from Day 6.
+// It has all the common properties and methods.
 class Car {
   // --- Properties ---
   // This is the data that a Car object will hold.
@@ -44,6 +45,66 @@ class Car {
 } // End of the Car class
 
 
+// --- NEW SUBCLASS (CHILD) #1 ---
+// 'ElectricCar' is a special type of 'Car'.
+// It uses the 'extends' keyword to inherit from 'Car'.
+class ElectricCar extends Car {
+  
+  // 1. Add new properties that only ElectricCars have
+  int batteryLevel;
+
+  // 2. Create the child's constructor
+  // It takes all the parent's required info, PLUS its own new info.
+  ElectricCar(String color, String brand, int modelYear, this.batteryLevel)
+      // 3. 'super(...)' passes the parent's info "up" to the Car(..) constructor.
+      : super(color, brand, modelYear);
+  
+  // 4. Override a method from the parent
+  // We want a different 'startEngine' behavior for an electric car.
+  @override
+  void startEngine() {
+    if (isEngineOn) {
+      print('The $brand\'s systems are already on.');
+    } else {
+      isEngineOn = true;
+      print('... (systems online) ...'); // Silent start!
+      print('The $brand is now silently running.');
+    }
+  }
+
+  // 5. Add a new method that only ElectricCars have
+  void charge() {
+    batteryLevel = 100;
+    print('The $brand is charging. Battery is at 100%.');
+  }
+} // End of the ElectricCar class
+
+
+// --- NEW SUBCLASS (CHILD) #2 ---
+// 'Truck' is another special type of 'Car'.
+class Truck extends Car {
+  
+  // 1. Add a new property
+  double towingCapacity; // in tons
+
+  // 2. Create the constructor
+  Truck(String color, String brand, int modelYear, this.towingCapacity)
+      : super(color, brand, modelYear); // Pass info to parent
+
+  // 3. Override a method
+  @override
+  void honk() {
+    // Trucks have a louder honk!
+    print('The $color $brand says: HOOOONK!');
+  }
+
+  // 4. Add a new method
+  void haul() {
+    print('The $brand is hauling with a capacity of $towingCapacity tons.');
+  }
+} // End of the Truck class
+
+
 // --- Program Entry Point ---
 void main() {
   print('--- Building a Muscle Car ---');
@@ -71,5 +132,32 @@ void main() {
 
   print('My BMW: ${germanCar2.brand} (${germanCar2.color})');
   germanCar2.stopEngine();  // Output: The engine is already off.
+
+  // --- NEW DAY 7 CODE ADDED BELOW ---
+  
+  print('\n--- Creating an Electric Car (Day 7) ---');
+  // Create an 'ElectricCar' object.
+  ElectricCar myTaycan = ElectricCar('Ice Grey', 'Porsche Taycan', 2024, 93);
+  
+  print('\n--- Creating a Truck (Day 7) ---');
+  // Create a 'Truck' object.
+  Truck myTruck = Truck('Black', 'Ford F-150', 2023, 5.5);
+
+  print('\n--- Interacting with the Taycan (Day 7) ---');
+  // 'honk' is INHERITED from Car
+  myTaycan.honk();
+  // 'startEngine' is OVERRIDDEN by ElectricCar
+  myTaycan.startEngine();
+  // 'charge' is a NEW method in ElectricCar
+  myTaycan.charge();
+
+  print('\n--- Interacting with the Truck (Day 7) ---');
+  // 'startEngine' is INHERITED from Car
+  myTruck.startEngine();
+  // 'honk' is OVERRIDDEN by Truck
+  myTruck.honk();
+  // 'haul' is a NEW method in Truck
+  myTruck.haul();
 }
+
 
